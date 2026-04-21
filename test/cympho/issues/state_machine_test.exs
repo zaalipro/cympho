224011ae75b93528929970ed36862b2ef2c54043
CEO <ceo@paperclip.ing>
2026-04-21 06:38:03 +0000

    fix(state_machine_test): update to use correct 6-state kanban - Co-Authored-By: Paperclip <noreply@paperclip.ing>

--- test/cympho/issues/state_machine_test.exs
+++ test/cympho/issues/state_machine_test.exs
@@ -1,5 +1,4 @@
 defmodule Cympho.Issues.StateMachineTest do
-  use Cympho.DataCase, async: true
   use ExUnit.Case, async: true
 
   alias Cympho.Issues.StateMachine
@@ -10,12 +9,16 @@ defmodule Cympho.Issues.StateMachineTest do
       assert StateMachine.valid_transition?(:backlog, :todo) == true
     end
 
+    test "backlog to in_progress is valid" do
+      assert StateMachine.valid_transition?(:backlog, :in_progress) == true
+    end
+
     test "backlog to blocked is valid" do
       assert StateMachine.valid_transition?(:backlog, :blocked) == true
     end
 
-    test "backlog to in_progress is valid" do
-      assert StateMachine.valid_transition?(:backlog, :in_progress) == true
+    test "backlog to done is invalid" do
+      assert StateMachine.valid_transition?(:backlog, :done) == false
     end
 
     # todo transitions
@@ -119,12 +122,6 @@ defmodule Cympho.Issues.StateMachineTest do
 
     test "unknown status returns empty list" do
       assert StateMachine.valid_transitions(:unknown) == []
-    end
-  end
-
-  describe "can_transition?/2" do
-    test "is an alias for valid_transition?" do
-      assert StateMachine.can_transition?(:backlog, :todo) == StateMachine.valid_transition?(:backlog, :todo)
     end
   end
 
